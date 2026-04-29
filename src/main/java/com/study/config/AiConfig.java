1c4316a142f49f32720e6ba4f9ce6c08c3cbc7d3
Md Asad <mdasadseikh10@gmail.com>
2026-04-29 23:17:37 +0530

    new fix ollama changed to openAi

--- src/main/java/com/study/config/AiConfig.java
+++ src/main/java/com/study/config/AiConfig.java
@@ -1,7 +1,7 @@
 package com.study.config;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class AiConfig {
 
     @Bean
-    public ChatClient chatClient(OllamaChatModel ollamaChatModel) {
-        return ChatClient.builder(ollamaChatModel).build();
+    public ChatClient chatClient(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
     }
 }
